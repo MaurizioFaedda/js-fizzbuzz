abe6c90cc5ddf1cd9d0294de70e41e018eee1b48
Maurizio <maurizio.faedda1@gmail.com>
2020-10-20 15:35:18 +0200

    Creazioni di variabili per una facile modifica
    Non funziona correttamente

--- main.js
+++ main.js
@@ -4,14 +4,18 @@
 // al posto dei multipli di 5, deve stampare "Buzz"
 // al posto dei multipli sia di 3 che di 5, deve stampare "FizzBuzz"
 
+var numero1 = parseInt(prompt("Inserisci un divisore"));
+var numero2 = parseInt(prompt("Inserisci un secondo divisore"));
 
+var divisore1 = i % numero1;
+var divisore2 = i % numero2;
 
 for (var i = 1; i <= 100; i++) {
-    if ((i % 3 == 0) && (i % 5 == 0)) {
+    if ((divisore1 == 0) && (divisore2 == 0)) {
         console.log('FizzBuzz');
-    } else if (i % 3 == 0) {
+    } else if (divisore1 == 0) {
         console.log('Fizz');
-    } else if (i % 5 == 0) {
+    } else if (divisore2 == 0) {
         console.log('Buzz');
     } else {
         console.log(i);
